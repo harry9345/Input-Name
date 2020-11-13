3672ff8725cb995bab3e7b1a330632a8712e4827
hossein <hr.movahed6@gmail.com>
2020-11-13 10:09:04 +0200

    Input Name component

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @set-user="setNewUser"></user-data>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "harry",
+        age: 37
+      }
+    };
+  },
+  methods: {
+    setNewUser(name, age) {
+      this.user = {
+        name: name,
+        age: +age
+      };
+    }
+  }
+};
+</script>
+
+
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid rgb(62, 52, 242);
+  padding: 1rem;
+}
+input {
+  border: 1px solid rgb(62, 52, 242);
+}
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="setUser">
+    <label for="name">Name</label>
+    <input type="text" placeholder="Name" v-model="enterName" />
+    <label for="age">agge</label>
+    <input type="number" placeholder="Age" v-model="enterAge" />
+    <button>Set ne user</button>
+  </form>
+</template>
+<script>
+export default {
+  emits: ["set-user"],
+  data() {
+    return {
+      enterName: "",
+      enterAge: ""
+    };
+  },
+  methods: {
+    setUser() {
+      this.$emit("set-user", this.enterName, this.enterAge);
+    }
+  }
+};
+</script>
